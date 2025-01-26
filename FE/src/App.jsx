2ebd601b1268a1945ca653cd4a1a9567bc095392
Ritesh <riteshloura9090@gmail.com>
2025-01-26 12:37:33 +0530

    tailwindUI setup

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,10 +1,10 @@
-import First from "./first";
+import Example from "./first";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind CSS!</h1>
-      <First />
+      <Example />
     </div>
   );
 }
