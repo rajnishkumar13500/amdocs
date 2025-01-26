30811bb39d4a751f4ea56dba3ee46abbeace2541
Ritesh <riteshloura9090@gmail.com>
2025-01-26 10:49:03 +0530

    tailwind setup

--- FE/src/first.jsx
+++ FE/src/first.jsx
@@ -1,5 +1,9 @@
 const First = () => {
-  return <div className="h-50 w-50 bg-red-200">Hii</div>;
+  return (
+    <div className="h-50 w-50 bg-red-200">
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
+    </div>
+  );
 };
 
 export default First;
