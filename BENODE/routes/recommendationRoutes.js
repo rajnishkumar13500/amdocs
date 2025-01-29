39128cb81f2923ee03db01359bec4641c6aa4472
Rajnish Kumar <rajnishkumar13500@gmail.com>
2025-01-29 17:20:13 +0530

    added recommendation routes

--- BENODE/routes/recommendationRoutes.js
+++ BENODE/routes/recommendationRoutes.js
@@ -4,5 +4,8 @@ const recommendationController = require('../controllers/recommendationControlle
 const { authenticateToken } = require('../middleware/authMiddleware');
 
 router.get('/', authenticateToken, recommendationController.getRecommendations);
+router.post('/train', authenticateToken, recommendationController.trainModel);
+router.post('/ml-recommendations', authenticateToken, recommendationController.getMlRecommendations);
+router.get('/feature-importance', authenticateToken, recommendationController.getFeatureImportance);
 
 module.exports = router; 
